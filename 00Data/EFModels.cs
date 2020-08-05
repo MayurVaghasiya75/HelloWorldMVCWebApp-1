922942ccb06fef5ac46ac34405c496b478184187
DESKTOP-0PVI9SU\i7 Laptop <anilchauhari005@yahoo.com>
2020-08-05 14:52:19 +0930

    Add project files.

---
+++ 00Data/EFModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldMVCWebApp._00Data
+{
+    public class Book
+    {
+
+        public int BookId { get; set; }
+        [StringLength(50)]
+        [Required]
+
+        public string Title { get; set; }
+
+        public string PublisherTitle { get; set; }
+        public string Author { get; set; }
+
+    }
+
+   /* public class Author
+    {
+
+        public int BookId { get; set; }
+        [StringLength(50)]
+        [Required]
+
+        public string Title { get; set; }
+        public string AuthorX { get; set; }
+
+    }*/
+}
+
---
+++ Migrations/20200805050655_BookClassWithPublishedName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HelloWorldMVCWebApp.Migrations
+{
+    public partial class BookClassWithPublishedName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            MigrationBuilder.AddColumn<string>(
+                name: "PublisheTitle",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            MigrationBuilder.DropColumn<string>(
+                name: "PublisheTitle",
+                table: "Books",
+                nullable: true);
+        }
+    }
+}
